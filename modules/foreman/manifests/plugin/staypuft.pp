63b736bb810e5445563a0b4adb6a91ce53eab75b
Marek Hulan <mhulan@redhat.com>
2014-05-16 10:02:11 +0200

    Split logic among more hooks
    This makes first wizard to remember answers from previous installation.
    It detect default values for the first time.
    
    These changes requires kafo >= 0.6.0 to work.

--- modules/foreman/manifests/plugin/staypuft.pp
+++ modules/foreman/manifests/plugin/staypuft.pp
@@ -16,7 +16,38 @@
 # $gateway::              What is the gateway for this machine
 #
 # $dns::                  DNS forwarder to use as secondary nameserver
-class foreman::plugin::staypuft($configure_networking = true, $interface, $ip, $netmask, $gateway, $dns) {
+#
+# === Extra parameters:
+#
+# These parameters are not used for this class. They are placeholders for the installer
+# so it can store values in answer file
+#
+# $network::              Network address used when seeding subnet in Foreman
+#
+# $from::                 DHCP range first address, used for DHCP configuration and
+#                         during Foreman subnet seeding
+#
+# $to::                   DHCP range last address, used for DHCP configuration and
+#                         during Foreman subned seeding
+#
+# $domain::               DNZ zone, used for DNS server configuration and during Foreman
+#                         Domain seeding
+#
+# $base_url::             URL of Foreman instance used for smart proxy configuration
+#
+class foreman::plugin::staypuft(
+    $configure_networking = true,
+    $interface,
+    $ip,
+    $netmask,
+    $gateway,
+    $dns,
+    $network,
+    $from,
+    $to,
+    $domain,
+    $base_url
+) {
   validate_bool($configure_networking)
 
   case $::operatingsystem {
@@ -31,7 +62,7 @@ class foreman::plugin::staypuft($configure_networking = true, $interface, $ip, $
   package { $staypuft_name:
     ensure => installed,
     notify => Class['foreman::service'],
-  } 
+  }
 
   if ($configure_networking) {
     class { 'network::global':
