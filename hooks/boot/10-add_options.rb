72a2d83ec09a4f969baa8daad8f57691a8df3676
Marek Hulan <mhulan@redhat.com>
2014-05-30 11:46:17 +0000

    Add staypuft-client-installer
    Existing staypuft-installer now generates answer file for client
    installer. Client installer prepares puppet on target machine and
    registers itself in staypuft.

--- hooks/boot/10-add_options.rb
+++ hooks/boot/10-add_options.rb
@@ -1,1 +1,5 @@
-app_option '--provisioning-wizard', :flag, 'Should the interactive wizard ask for provisioning information and prepare foreman to provision machines accordingly?', :default => true
+wizard = kafo.config.app.has_key?(:provisioning_wizard) ? kafo.config.app[:provisioning_wizard] : true
+app_option '--[no-]provisioning-wizard', :flag, 'Should the interactive wizard ask for provisioning information and prepare foreman to provision machines accordingly?', :default => wizard
+
+client = kafo.config.app.has_key?(:staypuft_client_installer) ? kafo.config.app[:staypuft_client_installer] : false
+app_option '--[no-]staypuft-client-installer', :flag, 'Is this staypuft client registeration mode?', :default => client
--- hooks/post/10-setup_provisioning.rb
+++ hooks/post/10-setup_provisioning.rb
@@ -1,10 +1,11 @@
 if app_value(:provisioning_wizard)
+  require 'tmpdir'
   require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'foreman.rb')
   require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'base_seeder.rb')
   require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'provisioning_seeder.rb')
   require File.join(KafoConfigure.root_dir, 'hooks', 'lib', 'subscription_seeder.rb')
 
-  puts "Starting configuration..."
+  say "Starting configuration..."
 
   # we must enforce at least one puppet run
   logger.debug 'Running puppet agent to seed foreman data'
@@ -28,4 +29,32 @@ if app_value(:provisioning_wizard)
   pro_seeder = ProvisioningSeeder.new(kafo)
   pro_seeder.seed
   `foreman-rake db:seed`
+
+  say "Generating answer file for client installers..."
+  dump_file = Dir.tmpdir + '/staypuft-client-installer.answers.yaml'
+  begin
+    pub_key_path = param('sshkeypair', 'home').value + '/.ssh/id_rsa.pub'
+    pub_key = File.read(pub_key_path).split(' ')[1]
+  rescue => e
+    say "Could not read SSH public key from #{pub_key_path} - #{e.message}, answer file will be <%= color('broken', :bad) %>"
+    pub_key = 'broken'
+  end
+
+  answers = {
+      'puppet' => {
+          'server' => false,
+          'runmode' => 'none',
+          'puppetmaster' => pro_seeder.fqdn,
+      },
+      'foreman::plugin::staypuft_client' => {
+          'staypuft_public_ssh_key' => pub_key,
+      }
+  }
+  File.open(dump_file, 'w') { |file| file.write(YAML.dump(answers)) }
+
+
+  say "  To register existing machine to staypuft, perform following actions on that host"
+  say "    1. Install foreman-installer-staypuft"
+  say "    2. Copy local <%= color('#{dump_file}', :info) %> file to /etc/foreman/staypuft-client-installer.answers.yaml on target host"
+  say "    3. Run staypuft-client-installer"
 end
