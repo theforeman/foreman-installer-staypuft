85a66b3ca618287f7aacc8b79f6b10348d243d22
Marek Hulan <mhulan@redhat.com>
2014-05-30 11:46:17 +0000

    Add staypuft-client-installer
    Existing staypuft-installer now generates answer file for client
    installer. Client installer prepares puppet on target machine and
    registers itself in staypuft.
    
    Generates SSH Key for foreman-proxy user and configures the
    foreman-proxy service to use puppetssh
    
    It creates foreman .ssh directory and copies the private ssh key
    from the foreman-proxy user. This allows DynFlow tasks running as
    foreman user to ssh into host machines, allow tasks to test that ssh is
    fully configured.

--- hooks/lib/install_modules.sh
+++ hooks/lib/install_modules.sh
@@ -3,6 +3,7 @@
 TARGET=/etc/puppet/environments/production/modules/
 OFI_MODULES=/usr/share/openstack-foreman-installer/puppet/modules
 OS_MODULES=/usr/share/openstack-puppet/modules
+INSTALLER_MODULES=/usr/share/foreman-installer/modules
 
 if [ -d $OFI_MODULES -a -d $OS_MODULES ]; then
   # copy modules from packages
@@ -18,26 +19,33 @@ else
   # get stable modules for git repositories
   ASTAPOR_VERSION="origin/1_0_stable"
   OPENSTACK_MODULES_VERSION="origin/havana"
-  
+
   mkdir -p /tmp/foreman_installer_staypuft
   cd /tmp/foreman_installer_staypuft
   rm -rf /tmp/astapor /tmp/openstack-puppet-modules modules
   mkdir modules
-  
+
   echo "Cloning repositories"
   git clone https://github.com/redhat-openstack/astapor
   git clone --recursive https://github.com/redhat-openstack/openstack-puppet-modules
-  
+
   pushd astapor
   git reset --hard $ASTAPOR_VERSION
   popd
   pushd openstack-puppet-modules
   git reset --hard $OPENSTACK_MODULES_VERSION
   popd
-  
+
   mv astapor/puppet/modules/* modules
   mv openstack-puppet-modules/* modules
   rm -rf astapor openstack-puppet-modules
 
   mv modules/* $TARGET
+fi
+
+# make a copy of installer modules (installer update should not change anything in master)
+mod_name="foreman"
+mod="$INSTALLER_MODULES/$mod_name"
+if [ ! -d $TARGET/$mod_name ]; then
+  cp -r $mod $TARGET
 fi
