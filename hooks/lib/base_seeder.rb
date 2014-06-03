d8e3989217cd16b1c691b33ae129ef259f7a6129
Marek Hulan <mhulan@redhat.com>
2014-06-03 13:16:15 +0000

    Support RHEL7 as a default OS for provisioning
    When staypuft host is RHEL6 then we automatically configure
    RHEL7 as a default OS for provisioning.

--- hooks/lib/base_seeder.rb
+++ hooks/lib/base_seeder.rb
@@ -19,10 +19,16 @@ class BaseSeeder
 
   private
 
-
   def find_default_os(foreman_host)
-    @foreman.operating_system.show! 'id' => foreman_host['operatingsystem_id'],
-                                    :error_message => "operating system for #{@fqdn} not found, DB inconsitency?"
+    os = @foreman.operating_system.show! 'id' => foreman_host['operatingsystem_id'],
+                                         :error_message => "operating system for #{@fqdn} not found, DB inconsitency?"
+
+    if os['name'] == 'RedHat' && os['major'] == '6'
+      os = foreman.operating_system.show_or_ensure({'id' => 'RedHat 7.0',
+                                                    'name' => 'RedHat', 'major' => '7', 'minor' => '0',
+                                                    'family' => 'Redhat'}, {})
+    end
+    os
   end
 
   def find_foreman_host
