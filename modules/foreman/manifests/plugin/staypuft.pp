0d598aae62c73429e0ed99dce65139f4af291856
Marek Hulan <mhulan@redhat.com>
2014-06-17 10:55:09 +0200

    Fix BZ#1108906
    Separate network configuration into own puppetrun. That means that
    facter is up to date when the installation itself is run. Also we
    add related hosts record if needed.

--- modules/foreman/manifests/plugin/staypuft.pp
+++ modules/foreman/manifests/plugin/staypuft.pp
@@ -1,8 +1,11 @@
 # = Staypuft
 #
-# Configures networking according to values gathered by wizard but only if enabled
+# Installs staypuft package
 #
-# === Parameters:
+# === Extra parameters:
+#
+# These parameters are not used for this class. They are placeholders for the installer
+# so it can store values in answer file
 #
 # $configure_networking:: Should local networking be configured by installer?
 #                         type:boolean
@@ -16,11 +19,6 @@
 # $own_gateway::          What is the gateway for this machine
 #
 # $dns::                  DNS forwarder to use as secondary nameserver
-#
-# === Extra parameters:
-#
-# These parameters are not used for this class. They are placeholders for the installer
-# so it can store values in answer file
 #
 # $network::              Network address used when seeding subnet in Foreman
 #
@@ -65,54 +63,5 @@ class foreman::plugin::staypuft(
   package { $staypuft_name:
     ensure => installed,
     notify => Class['foreman::service'],
-  }
-
-  if ($configure_networking) {
-    class { 'network::global':
-      gateway => $own_gateway,
-    }
-
-    network::if::static { $interface:
-      ensure    => 'up',
-      ipaddress => $ip,
-      netmask   => $netmask,
-      dns1      => $ip,
-      dns2      => $dns,
-      peerdns   => true,
-    }
-
-    # We don't want to purge rules other than we specify below (could disable ssh etc)
-    resources { "firewall":
-      purge => false
-    } ->
-    # The Foreman server needs to accept DNS requests on this port when provisioning systems.
-    firewall { '53 accept - dns':
-      port   => '53',
-      proto  => 'tcp',
-      action => 'accept',
-    } ->
-    # The Foreman server needs to accept TFTP requests on this port when provisioning systems.
-    firewall { '69 accept - tftp':
-      port   => '69',
-      proto  => 'udp',
-      action => 'accept',
-    } ->
-    # The Foreman web user interface accepts connections on these ports.
-    firewall { '80 accept - apache':
-      port   => '80',
-      proto  => 'tcp',
-      action => 'accept',
-    } ->
-    firewall { '443 accept - apache':
-      port   => '443',
-      proto  => 'tcp',
-      action => 'accept',
-    } ->
-    # The Foreman server accepts connections to Puppet on this port.
-    firewall { '8140 accept - puppetmaster':
-      port   => '8140',
-      proto  => 'tcp',
-      action => 'accept',
-    }
   }
 }
---
+++ modules/foreman/manifests/plugin/staypuft_network.pp
@@ -0,0 +1,81 @@
+# = Staypuft Network
+#
+# Configures networking according to values gathered by wizard, it does following actions
+#   * configures gateway
+#   * configures interface's IP address, netmask and DNS servers
+#   * adds hosts record if missing
+#   * configures firewall in a non-destructive way (leaves all existing rules untouched)
+#
+# === Parameters:
+#
+# $interface::            Which interface should this class configure
+#
+# $ip::                   What IP address should be set
+#
+# $netmask::              What netmask should be set
+#
+# $gateway::              What is the gateway for this machine
+#
+# $dns::                  DNS forwarder to use as secondary nameserver
+#
+class foreman::plugin::staypuft_network(
+    $interface,
+    $ip,
+    $netmask,
+    $gateway,
+    $dns,
+) {
+
+  class { 'network::global':
+    gateway => $gateway,
+  }
+
+  network::if::static { $interface:
+    ensure    => 'up',
+    ipaddress => $ip,
+    netmask   => $netmask,
+    dns1      => $ip,
+    dns2      => $dns,
+    peerdns   => true,
+  }
+
+  host { $fqdn:
+    comment      => 'created by puppet class foreman::plugin::staypuft_network',
+    ip           => $ip,
+    host_aliases => $hostname
+  }
+
+  # We don't want to purge rules other than we specify below (could disable ssh etc)
+  resources { "firewall":
+    purge => false
+  } ->
+  # The Foreman server needs to accept DNS requests on this port when provisioning systems.
+  firewall { '53 accept - dns':
+    port   => '53',
+    proto  => 'tcp',
+    action => 'accept',
+  } ->
+  # The Foreman server needs to accept TFTP requests on this port when provisioning systems.
+  firewall { '69 accept - tftp':
+    port   => '69',
+    proto  => 'udp',
+    action => 'accept',
+  } ->
+  # The Foreman web user interface accepts connections on these ports.
+  firewall { '80 accept - apache':
+    port   => '80',
+    proto  => 'tcp',
+    action => 'accept',
+  } ->
+  firewall { '443 accept - apache':
+    port   => '443',
+    proto  => 'tcp',
+    action => 'accept',
+  } ->
+  # The Foreman server accepts connections to Puppet on this port.
+  firewall { '8140 accept - puppetmaster':
+    port   => '8140',
+    proto  => 'tcp',
+    action => 'accept',
+  }
+}
