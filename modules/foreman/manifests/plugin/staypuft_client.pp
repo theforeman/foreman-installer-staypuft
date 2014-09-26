17792b5ff712ff48603ce134f04f64c36c7f0eef
Jiri Stransky <jistr@redhat.com>
2014-09-26 11:01:54 +0200

    Remove puppetmaster param from staypuft_client.pp
    The param is not used there, and it is not set in the answerfile,
    which made it generate a default of local host's fqdn, which was
    confusing and incorrect.

--- modules/foreman/manifests/plugin/staypuft_client.pp
+++ modules/foreman/manifests/plugin/staypuft_client.pp
@@ -4,14 +4,11 @@
 #
 # === Parameters:
 #
-# $puppetmaster::               Puppetmaster url (usually staypuft host fqdn)
-#
 # $puppetssh_user::             A user to use when connecting using SSH
 #
 # $staypuft_ssh_public_key::    This key will be added to SSH authorized keys
 #
 class foreman::plugin::staypuft_client(
-  $puppetmaster   = $::fqdn,
   $puppetssh_user = 'root',
   $staypuft_ssh_public_key,
 ) {
