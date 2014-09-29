2b1c98d6ee7c4d34054cdef5a24b39dbd52e71d1
Jiri Stransky <jistr@redhat.com>
2014-09-29 11:18:56 +0200

    Wait for client registration to finish
    Add "--no-daemonize" to registration puppet run so that we wait for it
    to finish.
    
    Add "-l console" so that it actually writes the output to
    foreman-installer log as intended (instead of system journal).

--- hooks/post/10-register_in_staypuft.rb
+++ hooks/post/10-register_in_staypuft.rb
@@ -1,5 +1,6 @@
 if app_value(:staypuft_client_installer)
   # register in staypuft by triggering puppet
-  logger.debug 'starting registration process by puppet agent --onetime --tags no_such_tag'
-  logger.debug `puppet agent --onetime --tags no_such_tag`
+  command = 'puppet agent --onetime --no-daemonize -l console --tags no_such_tag'
+  logger.debug "starting registration process by #{command}"
+  logger.debug `#{command}`
 end
