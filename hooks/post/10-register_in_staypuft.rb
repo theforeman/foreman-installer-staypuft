b2bd9180ac0bf8dbf26350124808a20d6dc8bffe
Jiri Stransky <jistr@redhat.com>
2014-09-29 12:49:53 +0200

    Fail the installer if the registration puppet run fails

--- hooks/post/10-register_in_staypuft.rb
+++ hooks/post/10-register_in_staypuft.rb
@@ -1,6 +1,13 @@
 if app_value(:staypuft_client_installer)
-  # register in staypuft by triggering puppet
-  command = 'puppet agent --onetime --no-daemonize -l console --tags no_such_tag'
-  logger.debug "starting registration process by #{command}"
+  # Register in staypuft by triggering puppet.
+
+  # --usecacheonfailure because on registration the host will not be
+  # able to fetch its yaml (Foreman doesn't know about the host yet).
+  command = 'puppet agent --onetime --no-daemonize -l console --usecacheonfailure --tags no_such_tag 2>&1'
+  logger.debug "Starting registration process by #{command}"
   logger.debug `#{command}`
+  unless [0,2].include? $?
+    logger.error "Error while trying to register the client in Staypuft."
+    kafo.class.exit(102)
+  end
 end
